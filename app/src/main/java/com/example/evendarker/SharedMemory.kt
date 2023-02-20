f6dbe924cd8a65d9b411124869f9cdf744eb15f4
Martin <martinlee5159@gmail.com>
2023-02-19 20:41:58 -0800

    implement shared preferences

---
+++ app/src/main/java/com/example/evendarker/SharedMemory.kt
@@ -0,0 +1,29 @@
+package com.example.evendarker
+
+import android.content.Context
+import android.content.SharedPreferences
+import android.graphics.Color
+
+class SharedMemory(context: Context) {
+    private var sharedPref: SharedPreferences = context.getSharedPreferences("SCREEN_FILTER", Context.MODE_PRIVATE)
+
+    private fun getValue(key:String, value:Int): Int{
+        return sharedPref.getInt(key, value)
+    }
+
+    private fun setValue(key:String, value:Int){
+        return sharedPref.edit().putInt(key,value).apply()
+    }
+
+    fun getAlpha(): Int{
+        return getValue("alpha",0x33)
+    }
+
+    fun setAlpha(value: Int){
+        return setValue("alpha",value)
+    }
+
+    fun getColor(): Int {
+        return Color.argb(getAlpha(),0,0,0)
+    }
+}
